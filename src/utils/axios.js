849aea796a8c3579c7318bf3bbc4ea6bd8e568a6
Aashi Ashfak <150011814+aashiashfak@users.noreply.github.com>
2025-01-21 19:18:28 +0530

    rearranged dropdown path

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000/";
+// Set base URL based on the environment
+const isLocalhost = window.location.hostname === "localhost";
+const baseURL = isLocalhost
+  ? "http://localhost:8000/" 
+  : "https://health-care-backend-ivory.vercel.app/"; 
 
 // Axios instance for regular API calls
 const instance = axios.create({
-  baseURL: baseUrl,
+  baseURL,
 });
 
 export default instance;
